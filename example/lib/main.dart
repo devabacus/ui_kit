bd150d062c0eaa354b8d93a5dca721e216b6a89c
mrfrolk <ekaterita20@gmail.com>
2025-03-31 08:41:01 +0500

    1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:ui_kit/ui_kit.dart';
 
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyWidget());
+  }
+}
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
@@ -11,7 +24,7 @@ class MyWidget extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-        
+            // ElevatedButton(style: , onPressed: () {}, child: Text("test"))
             Text("Example ui_kit"),
             AppGap.xl(),
             ElevatedButton(onPressed: () {}, child: Text("test")),
